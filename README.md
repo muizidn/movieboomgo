544ff2047aaf85dfb6f3be38f5d12f01a7d93e43
muizidn <muiz.idn@gmail.com>
2020-08-26 22:25:26 +0700

    add Welcome

---
+++ README.md
@@ -0,0 +1,3 @@
+# MovieBOOM
+
+https://tutorialedge.net/golang/creating-restful-api-with-golang/
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome!!")
+	fmt.Println("Endpoint hit : Homepage")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
 
 func main() {
-  fmt.Println("Hello@W");
+	fmt.Println("Running...")
+	handleRequest()
 }
